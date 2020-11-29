9651a5ecb12e1c4833d8f7f21b3e92fec1a67abd
iamsainikhil <sainikhil.bheema@gmail.com>
2020-11-28 23:48:17 -0500

    add favicons and PWA manifest

--- components/Head.jsx
+++ components/Head.jsx
@@ -22,7 +22,6 @@ const Head = ({
         {/* Twitter */}
         <meta name='twitter:card' content='summary_large_image' key='twcard' />
         <meta name='twitter:creator' content={twitterHandle} key='twhandle' />
-
         {/* Open Graph */}
         <meta property='og:type' content='website' key='ogtype' />
         <meta property='og:url' content={pathUrl} key='ogurl' />
@@ -30,6 +29,28 @@ const Head = ({
         <meta property='og:site_name' content={siteName} key='ogsitename' />
         <meta property='og:title' content={title} key='ogtitle' />
         <meta property='og:description' content={description} key='ogdesc' />
+        {/* favicons */}
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#333333' />
+        <meta name='msapplication-TileColor' content='#00aba9' />
+        <meta name='theme-color' content='#333' />
         <title>
           {title} | {page} | {siteName}
         </title>
