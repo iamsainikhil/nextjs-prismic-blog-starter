d13e65340455a98c106d4b2be7979b45e0e16e93
iamsainikhil <sainikhil.bheema@gmail.com>
2020-11-28 23:51:25 -0500

    add measuring performance metric logic

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,12 @@ import '../styles/layout.scss'
 import '../styles/footer.scss'
 import '../styles/component.scss'
 
+// for more info on measuring app performance
+// visit https://nextjs.org/docs/advanced-features/measuring-performance
+export function reportWebVitals(metric) {
+  console.log(metric)
+}
+
 function MyApp({Component, pageProps}) {
   return (
     <ThemeProvider theme={theme}>
