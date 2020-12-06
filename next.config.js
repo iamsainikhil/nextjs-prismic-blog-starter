0a8d717bda44707725e9dec8e2480e95de5a476b
iamsainikhil <sainikhil.bheema@gmail.com>
2020-12-06 07:02:40 -0500

    add next image sizes

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ module.exports = withPWA({
   // for more info, visit https://nextjs.org/docs/basic-features/image-optimization#domains
   images: {
     domains: ['images.prismic.io', 'prismic-io.s3.amazonaws.com'],
+    // next/image support `srcSet` using the below deviceSizes
+    // for more info, visit https://nextjs.org/docs/basic-features/image-optimization#device-sizes
+    deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    // https://nextjs.org/docs/basic-features/image-optimization#image-sizes
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   // visit https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   // for more info about React strict mode
