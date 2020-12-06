53067aef8223c4b5a491a2455beb40c08b689679
Restyled.io <commits@restyled.io>
2020-12-06 12:08:10 +0000

    Restyled by prettier

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,19 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import {jsx} from 'theme-ui'
-import {SiNextDotJs} from 'react-icons/si'
-import {FaHeart} from 'react-icons/fa'
-import Icon from './Icon'
-import {trackGAEvent} from '../utils/googleAnalytics'
+import { jsx } from "theme-ui";
+import { SiNextDotJs } from "react-icons/si";
+import { FaHeart } from "react-icons/fa";
+import Icon from "./Icon";
+import { trackGAEvent } from "../utils/googleAnalytics";
 
 const Footer = () => {
   return (
     <footer
       sx={{
-        bg: 'muted',
-      }}>
-      <div className='footer'>
+        bg: "muted",
+      }}
+    >
+      <div className="footer">
         {/* <div className="links-row">
           <div>
             <a
@@ -58,40 +59,41 @@ const Footer = () => {
 
         <div
           style={{
-            textAlign: 'center',
-            margin: '0.25rem auto',
-            wordSpacing: '0.2rem',
-          }}>
-          Made with{' '}
+            textAlign: "center",
+            margin: "0.25rem auto",
+            wordSpacing: "0.2rem",
+          }}
+        >
+          Made with{" "}
           <FaHeart
-            style={{color: '#CC3D5C', marginBottom: '-0.25rem'}}
-            aria-label='Love'
-            title='Love'
-          />{' '}
-          using{' '}
+            style={{ color: "#CC3D5C", marginBottom: "-0.25rem" }}
+            aria-label="Love"
+            title="Love"
+          />{" "}
+          using{" "}
           <SiNextDotJs
             sx={{
-              color: 'text',
-              marginBottom: '-0.25rem',
+              color: "text",
+              marginBottom: "-0.25rem",
             }}
-            aria-label='NextJS'
-            title='NextJS'
-          />{' '}
-          and{' '}
-          <picture className='prismic-icon'>
-            <source type='image/webp' srcSet='/prismic-icon.webp' />
-            <source type='image/jpeg' srcSet='/prismic-icon.jpg' />
+            aria-label="NextJS"
+            title="NextJS"
+          />{" "}
+          and{" "}
+          <picture className="prismic-icon">
+            <source type="image/webp" srcSet="/prismic-icon.webp" />
+            <source type="image/jpeg" srcSet="/prismic-icon.jpg" />
             <img
-              src='/prismic-icon.jpg'
-              alt='Prismic'
-              aria-label='Prismic'
-              title='Prismic'
-              className='prismic-icon'
+              src="/prismic-icon.jpg"
+              alt="Prismic"
+              aria-label="Prismic"
+              title="Prismic"
+              className="prismic-icon"
             />
           </picture>
         </div>
 
-        <div className='social-row'>
+        <div className="social-row">
           {/* <Icon
             name="Facebook"
             url="https://facebook.com"
@@ -102,57 +104,58 @@ const Footer = () => {
           /> */}
 
           <Icon
-            name='Twitter'
-            url='https://twitter.com/iamsainikhil12'
+            name="Twitter"
+            url="https://twitter.com/iamsainikhil12"
             style={{
-              color: 'primary',
-              fontSize: '1.5rem',
+              color: "primary",
+              fontSize: "1.5rem",
             }}
           />
 
           <Icon
-            name='Medium'
-            url='https://medium.com/@iamsainikhil'
+            name="Medium"
+            url="https://medium.com/@iamsainikhil"
             style={{
-              color: 'primary',
-              fontSize: '1.5rem',
+              color: "primary",
+              fontSize: "1.5rem",
             }}
           />
 
           <Icon
-            name='LinkedIn'
-            url='https://www.linkedin.com/in/iamsainikhil'
+            name="LinkedIn"
+            url="https://www.linkedin.com/in/iamsainikhil"
             style={{
-              color: 'primary',
-              fontSize: '1.70rem',
-              marginBottom: '0.25rem',
+              color: "primary",
+              fontSize: "1.70rem",
+              marginBottom: "0.25rem",
             }}
           />
         </div>
 
-        <p style={{textAlign: 'center', marginTop: '0.5rem'}}>
-          © {new Date().getFullYear()}{' '}
+        <p style={{ textAlign: "center", marginTop: "0.5rem" }}>
+          © {new Date().getFullYear()}{" "}
           <a
-            href='https://iamsainikhil.com'
-            target='_blank'
-            rel='noreferrer noopener'
-            className='special-link'
-            aria-label='Portfolio'
-            title='Portfolio'
-            sx={{color: 'primary'}}
+            href="https://iamsainikhil.com"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="special-link"
+            aria-label="Portfolio"
+            title="Portfolio"
+            sx={{ color: "primary" }}
             onClick={() =>
               trackGAEvent(
-                'footer links',
+                "footer links",
                 `clicked on copyright link in Footer`,
-                'text click'
+                "text click"
               )
-            }>
+            }
+          >
             Sai&nbsp;Nikhil
           </a>
         </p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,36 +1,40 @@
-import {default as NextLink} from 'next/link'
-import Headroom from 'react-headroom'
-import {Styled, useThemeUI} from 'theme-ui'
+import { default as NextLink } from "next/link";
+import Headroom from "react-headroom";
+import { Styled, useThemeUI } from "theme-ui";
 // import { GoSearch } from "react-icons/go"
-import {FiSun, FiMoon} from 'react-icons/fi'
-import {trackGAEvent} from '../utils/googleAnalytics'
+import { FiSun, FiMoon } from "react-icons/fi";
+import { trackGAEvent } from "../utils/googleAnalytics";
 
 const Header = () => {
-  const {theme, colorMode, setColorMode} = useThemeUI()
+  const { theme, colorMode, setColorMode } = useThemeUI();
 
   return (
     <Headroom disableInlineStyles upTolerance={10} downTolerance={10}>
-      <header style={{background: `${theme.colors.muted}`}} className='header'>
-        <div className='header-content'>
+      <header
+        style={{ background: `${theme.colors.muted}` }}
+        className="header"
+      >
+        <div className="header-content">
           <div>
-            <Styled.h1 style={{margin: '0'}}>
-              <NextLink href='/' passHref>
+            <Styled.h1 style={{ margin: "0" }}>
+              <NextLink href="/" passHref>
                 <Styled.a
                   style={{
-                    textDecoration: 'none',
-                    fontFamily: 'Damion',
-                    letterSpacing: '0.15rem',
+                    textDecoration: "none",
+                    fontFamily: "Damion",
+                    letterSpacing: "0.15rem",
                   }}
                   onClick={() =>
-                    trackGAEvent('logo', `clicked on site logo`, 'link click')
+                    trackGAEvent("logo", `clicked on site logo`, "link click")
                   }
-                  rel='noreferrer noopener'>
+                  rel="noreferrer noopener"
+                >
                   Blog
                 </Styled.a>
               </NextLink>
             </Styled.h1>
           </div>
-          <div className='header-links'>
+          <div className="header-links">
             {/* <Styled.h2 style={{ margin: "0 1rem" }}>
               <Styled.a as={Link} to="/">
                 Tags
@@ -47,35 +51,37 @@ const Header = () => {
               />
             </p> */}
             <p>
-              {colorMode === 'light' ? (
+              {colorMode === "light" ? (
                 <span
-                  title='Switch to Dark Mode'
-                  aria-label='Switch to Dark Mode'>
+                  title="Switch to Dark Mode"
+                  aria-label="Switch to Dark Mode"
+                >
                   <FiSun
-                    className='theme-icon'
+                    className="theme-icon"
                     onClick={() => {
-                      setColorMode('dark')
+                      setColorMode("dark");
                       trackGAEvent(
-                        'toggle theme',
+                        "toggle theme",
                         `enabled dark theme`,
-                        'icon click'
-                      )
+                        "icon click"
+                      );
                     }}
                   />
                 </span>
               ) : (
                 <span
-                  title='Switch to Light Mode'
-                  aria-label='Switch to Light Mode'>
+                  title="Switch to Light Mode"
+                  aria-label="Switch to Light Mode"
+                >
                   <FiMoon
-                    className='theme-icon'
+                    className="theme-icon"
                     onClick={() => {
-                      setColorMode('light')
+                      setColorMode("light");
                       trackGAEvent(
-                        'toggle theme',
+                        "toggle theme",
                         `enabled light theme`,
-                        'icon click'
-                      )
+                        "icon click"
+                      );
                     }}
                   />
                 </span>
@@ -85,7 +91,7 @@ const Header = () => {
         </div>
       </header>
     </Headroom>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
