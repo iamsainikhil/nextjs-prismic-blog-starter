57f646802fbf4d5bb4d27659508353a5bbd46df5
iamsainikhil <sainikhil.bheema@gmail.com>
2021-02-15 12:56:58 -0500

    fix(document): prevent theme color flashing on initial page load

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+import {InitializeColorMode} from 'theme-ui'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <InitializeColorMode />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
