68a7b83f0177867b823631f61688c29fb2b54260
iamsainikhil <sainikhil.bheema@gmail.com>
2025-08-08 16:25:01 -0400

    chore: update font

--- components/Head.jsx
+++ components/Head.jsx
@@ -62,6 +62,9 @@ const Head = ({
         />
         <link rel='manifest' href='/manifest.webmanifest' />
         <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#333333' />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Damion&family=Fira+Code&family=Lato:ital,wght@0,400;0,700;1,300&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet" />
         <meta name='msapplication-TileColor' content='#00aba9' />
         <meta name='theme-color' content='#333' />
         <title>{pageTitle}</title>
